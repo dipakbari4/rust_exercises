bf05689cc12ac48e96a31b42f1c9b6f91cf0f48f
Dipak Bari <dipakbari4@gmail.com>
2021-08-27 16:42:33 +0530

    String Literals

---
+++ literal_strings.rs
@@ -0,0 +1,7 @@
+/**
+ * This program show about the use case of String Literals.
+ * String literals are the placeholder to show the value of referenced variable(s).
+ */
+fn main(){
+  print!("This is {} strings", "literal");
+}
