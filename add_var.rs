d59246d042884b9670c5b03b0e96ea406b5d311f
Dipak Bari <dipakbari4@gmail.com>
2021-08-28 18:14:06 +0530

    Adding two variables

---
+++ add_var.rs
@@ -0,0 +1,8 @@
+// This program demonstrates how to add two varialbes
+// and printing them using placeholders
+
+fn main() {
+  let number = 15;
+  let another_number = 20;  // Use snack case for variable declaration
+  print!("{}", number + another_number);
+}
