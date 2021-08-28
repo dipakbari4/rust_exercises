0f85c61c02edc0de3451da6cf0b52bdc3d993f20
Dipak Bari <dipakbari4@gmail.com>
2021-08-29 00:22:40 +0530

    Immutable variable example

---
+++ immutable_variable.rs
@@ -0,0 +1,13 @@
+fn main(){
+  let my_var = 23;
+
+  // printing out my_var variable value
+  print!("my_var: {}", my_var);
+
+  // Adding 14 to current value of my_var variable
+  // uncomment the below line and you'll get error
+  // to re-assigning value of immutably variable.
+  // my_var = my_var + 14; 
+
+  //print!("my_var: {}", my_var);
+}
