73999b0de7c6d625579358593c51a5dda97562eb
Dipak Bari <dipakbari4@gmail.com>
2021-08-30 00:52:52 +0530

    arithmetic operator example

---
+++ arithmetic_operator.rs
@@ -0,0 +1,19 @@
+// This program demonstrates the symbol(s) of arithmetic operator
+fn main() {
+  let mut number = 12;
+
+  println!("original {}", number);
+
+  number = number + 3;
+  println!("add {}", number);
+
+  number = number - 5;
+  println!("subtract {}", number);
+
+  number = number / 2;
+  println!("division {}", number);
+  
+  number = number * 6;
+  println!("multiplication {}", number);
+
+}
