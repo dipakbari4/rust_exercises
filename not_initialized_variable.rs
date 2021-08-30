642c2041c05c02e2836c09c59a90a9d3e0f378c0
Dipak Bari <dipakbari4@gmail.com>
2021-08-30 21:44:42 +0530

    not initialized variable example

---
+++ not_initialized_variable.rs
@@ -0,0 +1,9 @@
+/**
+ * This program won't be compile and will throw
+ * compilation error because we didn't initialize
+ * the variable 'number' by assigning value to it.
+ */
+fn main() {
+  let number;
+  print!("{}", number);
+}
