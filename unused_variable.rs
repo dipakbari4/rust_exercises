a51a291180296bcc38d10f3cf956e9718e62eb8a
Dipak Bari <dipakbari4@gmail.com>
2021-08-30 21:48:59 +0530

    unused variable | use case of underscore

---
+++ unused_variable.rs
@@ -0,0 +1,10 @@
+/**
+ * This program will demonstrate about the unused variable
+ * compilation warning
+ */
+
+ fn main() {
+   // So, we declared a variable and assigned a value to it
+   // but never used it.
+   let number = 12; // Give underscore before variable name to omit the warnings
+ }
