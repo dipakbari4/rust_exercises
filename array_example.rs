db03d48fa6f934606653dc165dbacf8b57b53d28
Dipak Bari <dipakbari4@gmail.com>
2021-08-31 17:11:36 +0530

    array example

---
+++ array_example.rs
@@ -0,0 +1,11 @@
+/**
+ * This program demonstrate a sample use case
+ * of arrays in rust.
+ * Note: All the elements should have the same
+ * data-type.
+ */
+fn main() {
+  let words = ["this", "community", "great", "is", "but", "have", "great", "courage", "dedication", "programming", "requires", "and"];
+
+  print!("{} {} {} {}", words[0], words[9], words[3], words[6]);
+}
