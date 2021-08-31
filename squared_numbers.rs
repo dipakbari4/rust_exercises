35d2c60dc3c055294bb4dbe107eed30d5a34b9d7
Dipak Bari <dipakbari4@gmail.com>
2021-08-31 16:47:50 +0530

    squared number using while loop

---
+++ squared_numbers.rs
@@ -0,0 +1,10 @@
+// This program will print the squared values using
+// while loop
+fn main() {
+  let mut number = 1;
+
+  while number <= 10{
+    print!("{} ", number * number);
+    number += 1;
+  }
+}
