1a73c45a6dd92d1e9a35b2a99a7410af0058c62a
Dipak Bari <dipakbari4@gmail.com>
2021-08-31 16:37:33 +0530

    if statement example

---
+++ if_statement.rs
@@ -0,0 +1,11 @@
+/**
+ * This program shows about the use of if statement 
+ * in Rust Programming.
+ */
+fn main() {
+  let number = 4;
+
+  if number > 0 {
+    print!("number is positive");
+  }
+}
