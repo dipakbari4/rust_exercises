9d447f831e43f6ed96036292f6b3b72ae8020b11
Dipak Bari <dipakbari4@gmail.com>
2021-09-05 20:59:27 +0530

    Exponential Notation

---
+++ exponential_notation.rs
@@ -0,0 +1,10 @@
+// This program will print exponential notation
+
+fn main() {
+  let one_thousand = 1e3;
+  let one_million = 1e6;
+  let one_billion = 1e9;
+
+  print!("{} {} {}",
+  one_thousand, one_million, one_billion);
+}
