e0175fe545f005836b9f4afd0c7d28b2ad0d8a3a
Dipak Bari <dipakbari4@gmail.com>
2021-09-05 20:36:23 +0530

    numeric bases

---
+++ numeric_bases.rs
@@ -0,0 +1,10 @@
+// This program will print different bases
+
+fn main() {
+  let hexadecimal = 0x10;
+  let decimal = 10;
+  let octal = 0o10;
+  let binary = 0b10;
+
+  print!("{} {} {} {}", hexadecimal, decimal, octal, binary);
+}
