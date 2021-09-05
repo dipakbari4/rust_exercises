558ae722a57341e0a2fc56196edf78b55580b3ad
Dipak Bari <dipakbari4@gmail.com>
2021-09-05 20:53:35 +0530

    numerical literals

---
+++ numerical_literals.rs
@@ -0,0 +1,10 @@
+// Numerical literals
+fn main() {
+  let hexadecimal = 0x_00FF_F7A3;
+  let decimal = 1_234_567;
+  let octal = 0o_777_205_162;
+  let binary = 0b_0110_1001_1111_0001;
+
+  print!("{} {} {} {}",
+  hexadecimal, decimal, octal, binary);
+}
