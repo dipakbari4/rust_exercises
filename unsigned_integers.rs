1c3dca8497dda1f39835fcda607b5e9874707385
Dipak Bari <dipakbari4@gmail.com>
2021-09-06 22:12:58 +0530

    unsigned integers

---
+++ unsigned_integers.rs
@@ -0,0 +1,12 @@
+/**
+ * This program will demonstrate about
+ * unsigned integers
+ */
+
+ fn main() {
+   let a: u8 = 5;
+   let b: u16 = 5;
+   let c: u32 = 5;
+   let d: u64 = 5;
+   print!("{} {} {} {}", a, b, c, d);
+ }
