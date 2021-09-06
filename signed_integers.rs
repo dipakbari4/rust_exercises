f14880c87f833b7ef5af5f168c4ab1c650ad50b0
Dipak Bari <dipakbari4@gmail.com>
2021-09-06 21:56:53 +0530

    Signed Integers

---
+++ signed_integers.rs
@@ -0,0 +1,12 @@
+/**
+ * This program will demonstrate about the signed integers
+ * declarations and usage
+ */
+fn main() {
+  let a: i8 = 5;
+  let b: i16 = 5;
+  let c: i32 = 5;
+  let d: i64 = 5;
+
+  print!("{} {} {} {}", a, b, c, d);
+}
