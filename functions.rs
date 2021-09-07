a905ee9607167b4a8092330dd2a3c8876ff9cf52
Dipak Bari <dipakbari4@gmail.com>
2021-09-07 19:19:53 +0530

    function example

---
+++ functions.rs
@@ -0,0 +1,12 @@
+/**
+ * This program demonstrates the usage of functions
+*/
+
+fn main() {
+  println!("Calling functions");
+  sample_function();
+}
+
+fn sample_function() {
+  println!("This is a sample function");
+}
