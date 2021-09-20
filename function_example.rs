ce1ef071d91bee0eb61123903b98cf2b728d2da4
Dipak Bari <dipakbari4@gmail.com>
2021-09-20 20:30:07 +0530

    Function example

---
+++ function_example.rs
@@ -0,0 +1,14 @@
+/**
+ * In this program we'll see the demonstration
+ * of defining function and calling it in somewhere
+ */
+
+fn main() {
+  println!("Chapter 1");
+  line();
+  println!("Chapter 2");
+}
+
+fn line() {
+  println!("----------------------");
+}
